bff8b0f36dd0bfe98ac661efebb17ba36593b8c7
Ingernon <44840616+Ingernon@users.noreply.github.com>
2019-10-26 03:34:05 +0800

    cleaner
    cleaning to implemente security

--- Main.py
+++ Main.py
@@ -1,30 +1,26 @@
-from flask import Flask, render_template, Response, request
-from Capture import VideoFeed
-import time
-import User_imput
+import flask as fl
 from ast import literal_eval
+import time
 
+from Capture import VideoFeed
+import User_imput
 
-def loop(camera):
+def loop(feed):
 	while True:
 		start_time = time.time()
-		frame = camera.get_frame()
+		frame = feed.get_frame()
 		yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 		time.sleep(max(1./30 - (time.time() - start_time), 0))
 
-app = Flask(__name__)
+app = fl.Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('webtop.html')
-
-@app.route('/getimput')
-def getimput():
-	return render_template('app/getimput.js')
+	return fl.render_template('webtop.html')
 
 @app.route('/postkeys', methods=['GET', 'POST'])
 def postkeys():
-	rep = literal_eval(str(request.data).split("\'")[1])
+	rep = literal_eval(str(fl.request.trusted_hosts).split("\'")[1])
 	ui = User_imput.User_imput()
 	ui.keys = rep["keys"]
 	ui.mouse = rep["mouse"]
@@ -34,7 +30,7 @@ def postkeys():
 
 @app.route('/video_feed')
 def video_feed():
-	return Response(loop(VideoFeed()), mimetype='multipart/x-mixed-replace; boundary=frame')
+	return fl.Response(loop(VideoFeed()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
 	app.run(host='localhost', debug=True)
